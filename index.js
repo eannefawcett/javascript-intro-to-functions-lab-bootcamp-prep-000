9575050a73cbc6d5dbce14853771826cb74e4fa1
Elizabeth Fawcett <elizabethfawcett47@gmail.com>
2019-06-25 17:19:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,19 @@ function logWhisper (string) {
   console.log('hello')
 }
 function sayHiToGrandma (string) {
+<<<<<<< HEAD
   if (string === string.toLowerCase ()) {
   return "I can't hear you!";
    } else if (string === string.toUpperCase()) {
   return "YES INDEED!";
   } else if ("I love you, Grandma.") {
+=======
+  if (string) === string.toLowerCase () {
+  return "I can't hear you!";
+   } else if (string) === string.toUpperCase()
+  return "YES INDEED!";
+  } else if ("I love you, Grandma.")
+>>>>>>> ef2e5c1e0f555bba3f4426317581a929e8b9b62d
   return "I love you, too.";
   }
 }
